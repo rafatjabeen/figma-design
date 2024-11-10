c58124cbfbf45454aee4c2d74bc0e66cc1ebfd60
Rafat Jabeen <rafatjabeen123@hotmail.com>
2024-11-10 22:51:24 +0500

    first commit

---
+++ components/Header.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <div className= "w-[1920px] h-[80px] bg-[#A29875]  text-[45px] text-white mt-6 pt-2 pl-4">MANZZARI </div>
+
+  )
+}
+
+export default Header;
